a6ac807c90bd40fac2d9d6aabf8de33c0a0f19c8
姜杉 <jiangshan@metrodata.tech>
2022-06-30 16:35:27 +0800

    init

---
+++ tag.js
@@ -0,0 +1,36 @@
+const shell = require("shelljs");
+const fs = require("fs");
+const [tagName, product] = process.argv.slice(2);
+
+// datlas脚本修改
+const isDatlas = (product) => product === "datlas";
+
+const datlas = () => {
+  if (fs.existsSync("src/version")) {
+    fs.writeFile("src/version", tagName, function (err) {
+      if (err) {
+        return console.error(err);
+      }
+    });
+  } else {
+    console.error("src/version not exist");
+  }
+};
+
+const updateVersion = () => {
+  shell.exec("git co main");
+  shell.exec("git pull");
+  shell.exec(`git co -b ${tagName}`);
+
+  // 执行文件修改
+  isDatlas(product) && datlas();
+
+  shell.exec("git add .");
+  shell.exec(`git commit -m${tagName}`);
+  shell.exec(`git tag ${tagName}`);
+  shell.exec(`git push origin ${tagName}`);
+  shell.exec("git co main");
+  shell.exec(`git branch -D ${tagName}`);
+};
+
+updateVersion();
