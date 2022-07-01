92812fa006d7ae62c13be6fd9d9dc8428dfadf1b
姜杉 <jiangshan@metrodata.tech>
2022-07-01 11:29:23 +0800

    提交脚本

--- tag.js
+++ tag.js
@@ -6,17 +6,122 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+const [env, product, tagName] = process.argv.slice(2);
+const _branchName = `${product}-${tagName}`;
 
-// 目前支持的产品
-const DATLAS = "datlas";
-
-const datlas = () => {
-  const paths = [
-    `config/${env}/${product}/version`,
+// 支持的产品以及对应路径
+const productsWithPaths = {
+  datlas: [
+    `config/${env}/datlas/version`,
     `config/${env}/page_sharing/version`,
     `config/${env}/page_sharing_404/version`,
     `config/${env}/personalized_login/version`,
-  ];
+  ],
+  dataFactory: [`config/${env}/data-factory/version`],
+  dataMarket: [`config/${env}/data-market/version`],
+  microMdt: [`config/${env}/micro-mdt/version`],
+  myData: [`config/${env}/my-data/version`],
+  sso: [`config/${env}/sso/version`],
+};
+
+// 运行前检查
+const checkRun = (fn) => {
+  gitCheck();
+  tagNameCheck();
+  productCheck();
+  branchCheck();
+
+  fn();
+};
+
+// 更新后远程提交tag
+const updateVersionTag = () => {
+  // script
+  try {
+    if (shell.exec("git status").stdout.indexOf("working tree clean") === -1) {
+      clearConsole();
+      shell.exec("git status -s");
+
+      rl.question(
+        `如上所示, 本次改动将会合并到 ${product} 产品的tag(${tagName})中, 请检查提交文件询问是否继续（Y/N）\n`,
+        function (prompt) {
+          if (prompt?.toLowerCase() === "y") {
+            consoleSuccess("确认提交, 脚本继续执行");
+            rl.close();
+            execExtand("git pull");
+            execExtand(`git co -b ${_branchName}`);
+            // 执行文件修改
+            reWrite(productsWithPaths[product])();
+
+            execExtand("git add .");
+            execExtand(`git commit -m${tagName}`);
+            execExtand(`git tag ${tagName}`);
+            execExtand(`git push origin ${tagName}`);
+            execExtand("git co main");
+            execExtand(`git branch -D ${_branchName}`);
+
+            consoleSuccess(
+              `----------------------\n更新成功！\n目标产品：${product}\ntag版本：${tagName}\n----------------------`
+            );
+          } else if (prompt?.toLowerCase() === "n") {
+            consoleWarn("放弃提交，脚本结束");
+            rl.close();
+          } else {
+            consoleWarn("输入异常，请重新执行脚本");
+            rl.close();
+          }
+        }
+      );
+    }
+  } catch (error) {
+    consoleError(error);
+  }
+};
+
+const init = () => {
+  checkRun(updateVersionTag);
+};
+
+// -------↓↓↓ private function ↓↓↓----------
+const tagNameCheck = () => {
+  if (!tagName) {
+    consoleError(
+      "----------------------\n缺少tag\n尝试执行例如：yarn tag_deploy-dev-datlas dev_all_20220202\n----------------------"
+    );
+    shell.exit(1);
+  }
+};
+
+const productCheck = () => {
+  if (!product || !Object.values(productsWithPaths).includes(product)) {
+    consoleError(
+      `----------------------\n不支持该产品！\n----------------------`
+    );
+    shell.exit(1);
+  }
+};
+
+const branchCheck = () => {
+  // 检查是否为主分支
+  if (
+    shell.exec("git rev-parse --abbrev-ref HEAD").stdout.indexOf("main") === -1
+  ) {
+    consoleError(
+      "----------------------\n当前分支必须为main, 请检查\n----------------------"
+    );
+    shell.exit(1);
+  }
+};
+const gitCheck = () => {
+  // git command check
+  if (!shell.which("git")) {
+    //在控制台输出内容
+    consoleError("Sorry, this script requires git");
+    shell.exit(1);
+  }
+};
+
+const reWrite = (paths) => {
   paths.forEach((path) => {
     if (fs.existsSync(path)) {
       fs.writeFileSync(path, tagName, function (err) {
@@ -32,69 +137,7 @@ const datlas = () => {
     }
   });
 };
-
-const handlerWithProducts = {
-  [DATLAS]: datlas,
-};
-
-const [env, product, tagName] = process.argv.slice(2);
-const branchName = `${product}-${tagName}`;
-
-const updateVersion = () => {
-  // git command check
-  if (!shell.which("git")) {
-    //在控制台输出内容
-    consoleError("Sorry, this script requires git");
-    shell.exit(1);
-  }
-  if (!tagName) {
-    consoleError(
-      "----------------------\n缺少tag\n尝试执行例如：yarn tag_deploy-dev-datlas dev_all_20220202\n----------------------"
-    );
-    shell.exit(1);
-  }
-  // script
-  try {
-    if (shell.exec("git status").stdout.indexOf("working tree clean") === -1) {
-      clearConsole();
-      shell.exec("git status -s");
-      rl.question(
-        `如上所示, 本次改动将会合并到 ${product} 产品的tag(${tagName})中, 请检查提交文件询问是否继续（Y/N）\n`,
-        function (prompt) {
-          if (prompt?.toLowerCase() === "y") {
-            consoleSuccess("确认提交, 脚本继续执行");
-            rl.close();
-          } else if (prompt?.toLowerCase() === "n") {
-            consoleWarn("放弃提交，脚本结束");
-            rl.close();
-
-            execExtand("git co main");
-            execExtand("git pull");
-            execExtand(`git co -b ${branchName}`);
-            // 执行文件修改
-            handlerWithProducts[product]?.();
-
-            execExtand("git add .");
-            execExtand(`git commit -m${tagName}`);
-            execExtand(`git tag ${tagName}`);
-            execExtand(`git push origin ${tagName}`);
-            execExtand("git co main");
-            execExtand(`git branch -D ${branchName}`);
-
-            consoleSuccess(
-              `----------------------\n更新成功！\n目标产品：${product}\ntag版本：${tagName}\n----------------------`
-            );
-          } else {
-            consoleWarn("输入异常，请重新执行脚本");
-            rl.close();
-          }
-        }
-      );
-    }
-  } catch (error) {
-    consoleError(error);
-  }
-};
+// -------↑↑↑ private function ↑↑↑----------
 
 // -------↓↓↓ common ↓↓↓----------
 // 错误console
@@ -104,9 +147,7 @@ const consoleWarn = (err) => console.log("\x1B[33m", err);
 // 成功console
 const consoleSuccess = (success) => console.log("\x1B[36m%s\x1B[0m", success);
 
-/**
- * clear
- */
+// clear Terminal
 const clearConsole = function () {
   process.stdout.cursorTo(0, 0);
   process.stdout.clearScreenDown();
@@ -120,4 +161,5 @@ const execExtand = (echo) => {
 };
 // -------↑↑↑ common ↑↑↑----------
 
-updateVersion();
+// 执行init
+init();
