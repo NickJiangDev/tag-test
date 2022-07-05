9d041c2cc0c29f42ed933de6a793ee40f306f73d
gitlab <gitlab-ci@idatatlas.com>
2022-07-05 14:55:06 +0800

    update

--- push.js
+++ push.js
@@ -1,6 +1,7 @@
 const shell = require("shelljs");
 const fs = require("fs");
 
+shell.exec("git co test");
 fs.writeFileSync("config/test", "test1");
 shell.exec("git add .");
 shell.exec('git commit -m"test"');
